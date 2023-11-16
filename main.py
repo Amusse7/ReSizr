f02c9cadd7f099d440fac35b2d1233e05eca0212
Abdulkadir Musse <72959571+Amusse7@users.noreply.github.com>
2023-11-16 04:56:04 -0500

    user interaction added

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from PIL import Image
 
-image = Image.open('ReSizr.png')
-print(f"Current Size: {image.size}")
+def resize_image(size1, size2):
+    image = Image.open('ReSizr.png')
+    print(f"Current Size: {image.size}")
 
-resized_image = image.resize((250, 250))
-resized_image.save('ReSizr_resized.png')
+    resized_image = image.resize((size1, size2))
+    resized_image.save('ReSizr_resized_'+ str(size1) +'.png')
+
+size1 = int(input('enter width:'))
+size2 = int(input('enter height:'))
+resize_image(size1, size2)
